16bcf6029958a2e12ecc65f4e5ef049818f1a608
Manuel Ortega <manuel@lasindias.coop>
2018-04-21 23:05:05 -0500

    Added an space after related words when presenting them on a text

--- app/models/begrip.rb
+++ app/models/begrip.rb
@@ -9,12 +9,12 @@ class Begrip < ActiveRecord::Base
   default_scope { order :name }
 
   def _presentation
-    name + related_words_text('(',')')
+    name + related_words_text('(', ')')
   end
 
   def related_words_text(start_text='', end_text='')
     return '' if related_words.empty?
-    "#{start_text}#{related_words.map(&:name).join(',')}#{end_text}"
+    "#{start_text}#{related_words.map(&:name).join(', ')}#{end_text}"
   end
 
   validates :name, :presence => true
